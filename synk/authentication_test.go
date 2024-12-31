73b7d279559301dc15be4d5f69368a1ef4ecb0f3
Carl Meijer <hammingweight@gmail.com>
2024-12-31 06:40:30 +0200

    Start on Authenticate tests.

---
+++ synk/authentication_test.go
@@ -0,0 +1,31 @@
+package synk
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func mockServer(h http.HandlerFunc) (string, func()) {
+	ts := httptest.NewServer(h)
+
+	return ts.URL, func() {
+		ts.Close()
+	}
+}
+
+func TestAuthenticate(t *testing.T) {
+	tests := []struct {
+		Name string
+	}{{Name: "BadResponseCode"}}
+	for _, tc := range tests {
+		t.Run(tc.Name, func(t *testing.T) {
+			url, cleanup := mockServer(func(w http.ResponseWriter, r *http.Request) {
+
+			})
+			defer cleanup()
+			fmt.Println(url)
+		})
+	}
+}
